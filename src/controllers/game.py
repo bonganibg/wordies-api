572b276392d2aa9efa10015aee6dda3c2da86681
Bonganibg <bongani.gumbo@yahoo.com>
2024-03-20 17:02:53 +0200

    _

--- src/controllers/game.py
+++ src/controllers/game.py
@@ -11,7 +11,7 @@ class GameController():
     def get_round_details(self, level: int) -> list:
         letters = self.__game_logic.generate_valid_random_letters(level)
         possible_words = self.__game_logic.get_total_possible_words(letters)
-        return Round(letters=letters, possilbe_words_count=possible_words, level=level)
+        return Round(letters=letters, possible_words_count=possible_words, level=level)
     
     def get_score(self, attempt: Attempt) -> int:
         return self.__game_logic.get_score(attempt.word, attempt.letters)
